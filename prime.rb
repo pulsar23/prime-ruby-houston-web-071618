20d63fb9850d73eca1a58efe1437579181b6118d
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-07 22:58:41 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
 def prime?(number)
   max_number = number -1
-  if number < 0 
+  if number < 1 
     return FALSE
-  elsif number == 1 
-  end
-  for x in 2..max_number
-    if number%x == 0
-      return FALSE
+  else
+    for x in 2..max_number
+      if number%x == 0
+        return FALSE
+      end
     end
   end
   #If not divisible by any number other than 1 or itself, is a prime
