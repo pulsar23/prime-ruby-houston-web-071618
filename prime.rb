4c4bccccea7787dd49fa844a42a066ba39d62f27
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-07 23:01:32 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(number)
+  max_number = number -1
+  if number <= 1 
+    return FALSE
+  else
+    for x in 2..max_number
+      if number%x == 0
+        return FALSE
+      end
+    end
+  end
+  #If not divisible by any number other than 1 or itself, is a prime
+  return TRUE
+end
